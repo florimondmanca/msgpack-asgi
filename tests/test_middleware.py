d5a0561c37413cd53a0053c92c69633050fcd0b5
Florimond Manca <florimond.manca@gmail.com>
2020-03-26 21:03:25 +0100

    Update tooling (#3)

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -10,7 +10,8 @@ from msgpack_asgi.responses import MessagePackResponse
 from tests.utils import mock_receive, mock_send
 
 
-def test_msgpack_request() -> None:
+@pytest.mark.asyncio
+async def test_msgpack_request() -> None:
     async def app(scope: Scope, receive: Receive, send: Send) -> None:
         request = Request(scope, receive=receive)
         content_type = request.headers["content-type"]
@@ -23,17 +24,18 @@ def test_msgpack_request() -> None:
 
     app = MessagePackMiddleware(app)
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
         content = {"message": "Hello, world!"}
         body = msgpack.packb(content)
-        r = client.post(
+        r = await client.post(
             "/", data=body, headers={"content-type": "application/x-msgpack"}
         )
         assert r.status_code == 200
         assert r.text == "content_type='application/x-msgpack' message='Hello, world!'"
 
 
-def test_non_msgpack_request() -> None:
+@pytest.mark.asyncio
+async def test_non_msgpack_request() -> None:
     async def app(scope: Scope, receive: Receive, send: Send) -> None:
         request = Request(scope, receive=receive)
         content_type = request.headers["content-type"]
@@ -46,17 +48,18 @@ def test_non_msgpack_request() -> None:
 
     app = MessagePackMiddleware(app)
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
-        r = client.post("/", json={"message": "Hello, world!"})
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        r = await client.post("/", json={"message": "Hello, world!"})
         assert r.status_code == 200
         assert r.text == "content_type='application/json' message='Hello, world!'"
 
 
-def test_msgpack_accepted() -> None:
+@pytest.mark.asyncio
+async def test_msgpack_accepted() -> None:
     app = MessagePackMiddleware(JSONResponse({"message": "Hello, world!"}))
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
-        r = client.get("/", headers={"accept": "application/x-msgpack"})
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        r = await client.get("/", headers={"accept": "application/x-msgpack"})
         assert r.status_code == 200
         assert r.headers["content-type"] == "application/x-msgpack"
         expected_data = {"message": "Hello, world!"}
@@ -64,21 +67,23 @@ def test_msgpack_accepted() -> None:
         assert msgpack.unpackb(r.content, raw=False) == expected_data
 
 
-def test_msgpack_accepted_but_response_is_not_json() -> None:
+@pytest.mark.asyncio
+async def test_msgpack_accepted_but_response_is_not_json() -> None:
     app = MessagePackMiddleware(PlainTextResponse("Hello, world!"))
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
-        r = client.get("/", headers={"accept": "application/x-msgpack"})
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        r = await client.get("/", headers={"accept": "application/x-msgpack"})
         assert r.status_code == 200
         assert r.headers["content-type"] == "text/plain; charset=utf-8"
         assert r.text == "Hello, world!"
 
 
-def test_msgpack_accepted_and_response_is_already_msgpack() -> None:
+@pytest.mark.asyncio
+async def test_msgpack_accepted_and_response_is_already_msgpack() -> None:
     app = MessagePackMiddleware(MessagePackResponse({"message": "Hello, world!"}))
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
-        r = client.get("/", headers={"accept": "application/x-msgpack"})
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        r = await client.get("/", headers={"accept": "application/x-msgpack"})
         assert r.status_code == 200
         assert r.headers["content-type"] == "application/x-msgpack"
         expected_data = {"message": "Hello, world!"}
@@ -86,11 +91,12 @@ def test_msgpack_accepted_and_response_is_already_msgpack() -> None:
         assert msgpack.unpackb(r.content, raw=False) == expected_data
 
 
-def test_msgpack_not_accepted() -> None:
+@pytest.mark.asyncio
+async def test_msgpack_not_accepted() -> None:
     app = MessagePackMiddleware(JSONResponse({"message": "Hello, world!"}))
 
-    with httpx.Client(app=app, base_url="http://testserver") as client:
-        r = client.get("/")
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        r = await client.get("/")
         assert r.status_code == 200
         assert r.headers["content-type"] == "application/json"
         assert r.json() == {"message": "Hello, world!"}
