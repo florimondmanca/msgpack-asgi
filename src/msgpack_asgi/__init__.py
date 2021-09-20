6b8bc1dd4115409d61b6224ddefb53cc085bd995
florimondmanca <florimond.manca@protonmail.com>
2021-09-20 14:46:37 +0200

    Drop unadvertised MessagePackResponse component

--- src/msgpack_asgi/__init__.py
+++ src/msgpack_asgi/__init__.py
@@ -1,5 +1,4 @@
 from .__version__ import __version__
 from ._middleware import MessagePackMiddleware
-from ._responses import MessagePackResponse
 
-__all__ = ["__version__", "MessagePackMiddleware", "MessagePackResponse"]
+__all__ = ["__version__", "MessagePackMiddleware"]
